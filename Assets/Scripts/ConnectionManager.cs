dd4bd16f97efcdbd889e844a3e327aa6a0801920
ILLUTE <illute@icoud.com>
2021-08-08 16:22:35 +0530

    Added Connection Manager

---
+++ Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+public class ConnectionManager : IConnectionCallbacks,IMatchmakingCallbacks
+{
+    public void OnConnected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnConnectedToMaster()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnCreatedRoom()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnCreateRoomFailed(short returnCode, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnCustomAuthenticationFailed(string debugMessage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnCustomAuthenticationResponse(Dictionary<string, object> data)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDisconnected(DisconnectCause cause)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnFriendListUpdate(List<FriendInfo> friendList)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnJoinedRoom()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnJoinRandomFailed(short returnCode, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnJoinRoomFailed(short returnCode, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnLeftRoom()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnRegionListReceived(RegionHandler regionHandler)
+    {
+        throw new System.NotImplementedException();
+    }
+}
